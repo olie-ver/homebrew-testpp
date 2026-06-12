de0efdc14c5a96213e8c1459cb77983f1dba5aba
Oliver Lie <oliverlie@krystal3d.com>
2026-06-11 18:58:08 -0700

    Changed license type in tap

--- testpp.rb
+++ testpp.rb
@@ -3,7 +3,7 @@ class Testpp < Formula
   homepage "https://github.com/olie-ver/testpp"
   url "https://github.com/yourusername/testpp/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "MIT"
+  license "OSNCL"
 
   depends_on "cmake" => :build
 
