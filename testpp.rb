ea4a75eb437864341f680a49b9841b1070b5dbaf
Oliver Lie <oliverlie@krystal3d.com>
2026-06-11 18:57:49 -0700

    Changed homebrew tap

--- testpp.rb
+++ testpp.rb
@@ -1,8 +1,8 @@
 class Testpp < Formula
   desc "Modern C++ unit testing framework"
-  homepage "https://github.com/yourusername/testpp"
+  homepage "https://github.com/olie-ver/testpp"
   url "https://github.com/yourusername/testpp/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "cmake" => :build
