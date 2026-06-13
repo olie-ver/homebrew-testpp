f67001cc71d181a7ad0d04cb0d68b09d1852ff01
Oliver Lie <oliverlie@krystal3d.com>
2026-06-12 22:45:12 -0700

    Update sha256 checksum for Testpp formula

--- testpp.rb
+++ testpp.rb
@@ -2,7 +2,7 @@ class Testpp < Formula
   desc "Modern C++ unit testing framework"
   homepage "https://github.com/olie-ver/TestPlusPlus"
   url "https://github.com/olie-ver/TestPlusPlus/archive/refs/tags/v20.0.0.tar.gz"
-  sha256 "3d3b1acb03ad09db5a342b312d027e309f330b62615d0f3590ec521a61156c78"
+  sha256 "b8ef9178e26619fc9b3b207598b17fde442722a4be823c3e2271f66c25770582"
   license "OSNCL"
 
   depends_on "cmake" => :build
