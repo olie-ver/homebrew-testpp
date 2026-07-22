8ec878ff8808968c7b0ae7a9cad0cd66d79a44f3
Oliver Lie <oliverlie@krystal3d.com>
2026-07-21 22:01:11 -0700

    Update testpp.rb

--- testpp.rb
+++ testpp.rb
@@ -2,7 +2,7 @@ class Testpp < Formula
   desc "Modern C++ unit testing framework"
   homepage "https://github.com/olie-ver/TestPlusPlus"
   url "https://github.com/olie-ver/TestPlusPlus/archive/refs/tags/v20.0.4.tar.gz"
-  sha256 "b2d245f880e35d9993bb7543472d4cc3db152ca5b75592f518a8372e4b5b7e32"
+  sha256 "cdb0f8bb47bd82f1231f8171c2e2e3392518d1f3485572f69e36baec082e20ad"
   license "OSNCL"
 
   depends_on "cmake" => :build
